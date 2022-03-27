4cdcd5e55c9e3b20b096435ffa73c20210062376
CalebNoel <caleb.a.noel@colorado.edu>
2022-03-27 02:46:00 -0600

    Changed secret key and put in .env file

--- clashstats/settings.py
+++ clashstats/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+1@!l=+w3bwf7pk$n_$&il5=54m2=9mi3ch!z74301p&lth3%9'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- stats/services.py
+++ stats/services.py
@@ -1,8 +1,11 @@
 import urllib
 import json
+import environ
+env = environ.Env()
+environ.Env.read_env()
 
 def get_cards():
-    key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFlMzQyYWJmLTRkZTctNDkwMC1hNTgwLWQ4YjM5YmM5NGZhZSIsImlhdCI6MTY0ODE5NDk4Nywic3ViIjoiZGV2ZWxvcGVyL2ZjMmM2MWFhLWM2MzQtZjViMi04YjcyLTZkMDU0ODkzODFjOCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3My43OC4xMzMuOTYiXSwidHlwZSI6ImNsaWVudCJ9XX0.GYFXN5fduT87L5XvTG5CTDxxt0kLeExhXnN0QCcrvy9q64kU7TMG8sNm9EKyHIxRXHfEEeCxfDfcMxDqpFet2g"
+    key = env('API_KEY')
     base_url = "https://api.clashroyale.com/v1"
     endpoint = "/cards"
     request =  urllib.request.Request(base_url+endpoint, None, {"Authorization": "Bearer %s" % key})
@@ -10,7 +13,7 @@ def get_cards():
     return response
 
 def get_player_info(tag):
-    key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFlMzQyYWJmLTRkZTctNDkwMC1hNTgwLWQ4YjM5YmM5NGZhZSIsImlhdCI6MTY0ODE5NDk4Nywic3ViIjoiZGV2ZWxvcGVyL2ZjMmM2MWFhLWM2MzQtZjViMi04YjcyLTZkMDU0ODkzODFjOCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3My43OC4xMzMuOTYiXSwidHlwZSI6ImNsaWVudCJ9XX0.GYFXN5fduT87L5XvTG5CTDxxt0kLeExhXnN0QCcrvy9q64kU7TMG8sNm9EKyHIxRXHfEEeCxfDfcMxDqpFet2g"
+    key = env('API_KEY')
     base_url = "https://api.clashroyale.com/v1"
     tag = tag.replace('#', "", 1).upper()
     endpoint = "/players/%23" + tag
@@ -26,7 +29,7 @@ def get_player_info(tag):
         return response
 
 def get_player_battles(tag):
-    key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFlMzQyYWJmLTRkZTctNDkwMC1hNTgwLWQ4YjM5YmM5NGZhZSIsImlhdCI6MTY0ODE5NDk4Nywic3ViIjoiZGV2ZWxvcGVyL2ZjMmM2MWFhLWM2MzQtZjViMi04YjcyLTZkMDU0ODkzODFjOCIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyI3My43OC4xMzMuOTYiXSwidHlwZSI6ImNsaWVudCJ9XX0.GYFXN5fduT87L5XvTG5CTDxxt0kLeExhXnN0QCcrvy9q64kU7TMG8sNm9EKyHIxRXHfEEeCxfDfcMxDqpFet2g"
+    key = env('API_KEY')
     base_url = "https://api.clashroyale.com/v1"
     tag = tag.replace('#', "", 1).upper()
     endpoint = "/players/%23" + tag + "/battlelog"
